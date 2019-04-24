7e092c459c9e451c46f1b7121de5a6f713e824d1
Vikram Sakkia <vikram.sakkia@gmail.com>
2019-04-24 10:53:49 +0400

    Added notifs and bg usage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,7 +5,8 @@ import { DeviceOrientation } from '@ionic-native/device-orientation/ngx';
 import { LocalNotifications, ELocalNotificationTriggerUnit, ILocalNotificationActionType, ILocalNotification } from '@ionic-native/local-notifications/ngx';
 import { AlertController, Platform } from '@ionic/angular';
 import { BackgroundMode } from '@ionic-native/background-mode/ngx';
-
+import { HTTP } from '@ionic-native/http/ngx';
+import { EmptyError } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -19,7 +20,8 @@ export class Home {
     private plt: Platform, 
     private localNotifications: LocalNotifications, 
     private alertCtrl: AlertController,
-    private backgroundMode: BackgroundMode
+    private backgroundMode: BackgroundMode,
+    private http: HTTP
     ){
       this.plt.ready().then(() => {
         this.localNotifications.on('click').subscribe(res => {
@@ -40,15 +42,21 @@ export class Home {
     }
 
     scheduleNotification() {
-      console.log("notif");
-      this.localNotifications.schedule({
-        id: 1,
-        title: 'Attention',
-        text: 'Test Notification',
-        data: { mydata: 'My hidden message this is' },
-        trigger: { in: 1, unit: ELocalNotificationTriggerUnit.SECOND },
-        foreground: true // Show the notification while app is open
-      });        
+      this.http.get(('http://omaraa.ddns.net:62027/emergency'), {}, {}).then(
+            data => {
+              let parsedData = JSON.parse(data.data);
+              let emergency = parsedData['emergencyStatus'];
+              if (emergency){
+                this.localNotifications.schedule({
+                  id: 1,
+                  title: 'Emergency',
+                  text: 'Please evacuate immediately',
+                  trigger: { in: 1, unit: ELocalNotificationTriggerUnit.SECOND },
+                  foreground: true // Show the notification while app is open
+                });
+              }
+            }
+          );              
     }
 
     showAlert(header, sub, msg) {
